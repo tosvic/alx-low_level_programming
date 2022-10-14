9c7a8b5b65422061f73c1a6703d673c87051c9af
tosvic <victoruhuo@yahoo.com>
2022-10-13 20:35:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(1, quo, 59);
 	return (1);
 }
