ea3519964045f2dff2990d4128cf09a1fb7b4d52
tosvic <victoruhuo@yahoo.com>
2022-10-13 20:09:15 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(2, "nd that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+	fwrite(2, "nd that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
