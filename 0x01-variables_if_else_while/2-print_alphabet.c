71387b1f8138e7e1005f1b687bf5d3009c8e813d
tosvic <victoruhuo@yahoo.com>
2022-10-14 21:30:19 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Enrty point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+	putchar("\n");
+	return (0);
+}
