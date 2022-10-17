37d0726bcaa5ea849f54a4e9dbfc7ce4cd255c36
tosvic <victoruhuo@yahoo.com>
2022-10-17 11:05:18 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9 using putchar while using int variable
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 9)
+	{
+		putchar(dig + '0');
+		++dig;
+	}
+	putchar("\n");
+
+	return (0);
+}
