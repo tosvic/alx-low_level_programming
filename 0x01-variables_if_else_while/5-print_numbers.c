e9d544aec7e602725f57752e2768dfed16df9b0a
tosvic <victoruhuo@yahoo.com>
2022-10-17 10:53:32 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int dig = 0;
+
+	while (dig <= 9)
+	{
+		printf("%i", dig);
+		++dig;
+	}
+	printf("\n");
+
+	return (0);
+}
