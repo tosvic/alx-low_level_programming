e152100b53c07a4a9f1014273009fb0ff3aa461f
tosvic <victoruhuo@yahoo.com>
2022-10-19 10:05:28 -0700

    advance 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include"main.h"
+
+/**
+ * print_time_table - print time table of numbers
+ *
+ * @n: take input
+ */
+void print_times_table(int n)
+{
+	int prod, mult, num;
+
+	if (num <= 15 && >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for ((mult = 1; mult <= n; ++mult)
+					{
+					_putchar(',');
+					_putchar(' ');
+
+					prod = num * mult;
+
+					if (prod <= 9)
+					_putchar(' ');
+					{
+					if (prod <= 99)
+					_putchar(' ');
+
+					if (prod >= 100)
+					{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+					}
+					else if (prod <= 99 && prod >= 10)
+					_putchar((prob / 10) + 48)
+					_putchar((prob % 10) + 48);
+					}
+					_putchar('\n');
+					}
