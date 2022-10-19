73a59a7b0271ac94f15c93fa77eac862580b6341
tosvic <victoruhuo@yahoo.com>
2022-10-19 05:02:05 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ *main - Entry point
+ * Description: print_alphabet_x10 - function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ++line)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+	_putchar('\n');
+	}
+}
