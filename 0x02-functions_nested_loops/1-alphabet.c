855e1d8f572f40868a6816ec90dbec1f6f132f22
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-19 12:43:59 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
- * the alphabet a - z
- */
+ *                 the alphabet a - z
+*/
 
 void print_alphabet(void)
 {
-	int a;
+	int ch;
 
-	for (a = 'a'; a <= 'z'; ++a)
-		_putchar(a)
-			_putchar('\n');
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
 }
