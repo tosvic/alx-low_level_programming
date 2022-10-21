d85baff8d694342ecc97df12d3df9458290ef20e
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-21 05:35:48 +0100

    task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,21 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int num1;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		num1 = -n;
 		_putchar('-');
 	} else
 	{
-		n1 = n;
+		num1 = n;
 	}
 
-	if (n1 / 10)
+	if (num1 / 10)
 	{
-		print_number(n1 / 10);
+		print_number(num1 / 10);
 	}
 
-	_putchar((n1 % 10) + '0');
+	_putchar((num1 % 10) + '0');
 }
