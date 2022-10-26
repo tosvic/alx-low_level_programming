068c443d3b5dfd70d95e6a61d1d7eee44e3a3df4
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-26 02:36:31 +0100

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,30 @@
-include "main.h"
-/**
- * rev_string - reverses a string
- *
- * @s: string parameter input
- *
- * Return: Nothing
-*/
+#include "main.h"
 
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
 void rev_string(char *s)
 {
-	int l, i;
-	char ch;
+	int count = 0, i, j;
+	char *str, temp;
 
-	/*find string length without null char*/
-	for (l = 0; s[l] != '\0'; ++l)
-		;
-
-	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; ++i)
+	while (count >= 0)
 	{
-		ch = s[i];
-		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
-		s[l - 1 - i] = ch;
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
 }
