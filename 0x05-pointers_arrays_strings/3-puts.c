704cbfecc183a7af99d2e835bb9ddbe418cd94dd
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-26 02:03:41 +0100

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,37 +1,19 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _puts - prints a string, followed by a new line,
- *
- *   * @str: pointer to the string to print
- *
- *    * Return: void
- *
- *     */
-
-
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
 
 void _puts(char *str)
-
 {
+	int num = 0;
 
-		int i = 0;
-
-
-
-			while (str[i])
-
-					{
-
-								_putchar(str[i]);
-
-										i++;
-
-											}
-
-				_putchar('\n');
-
+	while (str[num])
+	{
+		_putchar(str[num]);
+		num++;
+	}
+	_putchar('\n');
 }
