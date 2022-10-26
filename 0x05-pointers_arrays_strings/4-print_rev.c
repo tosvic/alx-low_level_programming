c8957c339b12edb5ad763480f1ca8893d80d4782
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-26 02:06:54 +0100

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_rev - prints a string, in reverse, followed by a new line.
- *
- *   * @s: input string.
- *
- *    * Return: no return.
- *
- *     */
-
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
 void print_rev(char *s)
-
 {
+	int num = 0;
 
-		int count = 0;
+	while (num >= 0)
+	{
+		if (s[num] == '\0')
+			break;
+		num++;
+	}
 
-
-
-			while (count >= 0)
-
-					{
-
-								if (s[count] == '\0')
-
-												break;
-
-										count++;
-
-											}
-
-
-
-				for (count--; count >= 0; count--)
-
-							_putchar(s[count]);
-
-					_putchar('\n');
-
+	for (num--; num >= 0; num--)
+		_putchar(s[num]);
+	_putchar('\n');
 }
