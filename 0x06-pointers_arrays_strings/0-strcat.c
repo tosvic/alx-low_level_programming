e12b4aaebd006f545234cf3b6e28f4fceeeb9c8e
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-27 16:15:22 +0100

    task 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,27 @@
-#include "main.h"
 
 /**
- * *_strcat - function commute srtings
- * @dest: param pointer to a char
- * @src: param pointer to a char
- * Return: return value of dest
- */
+ * _strcat - function that concatenates
+ *          two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int c, c2;
 
-	i = 0;
-	j = 0;
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
 
-	dest[i] = '\0';
 	return (dest);
-}
+}                                                                                                                                    
