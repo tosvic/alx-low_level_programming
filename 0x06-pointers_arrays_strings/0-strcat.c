5795600a1d4212e55c76ef0e47b42b6522b7b667
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-27 16:08:28 +0100

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- *_strcat - function commute srtings
+ * *_strcat - function commute srtings
  * @dest: param pointer to a char
  * @src: param pointer to a char
  * Return: return value of dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int x;
