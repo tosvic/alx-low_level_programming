b2f6eaf67dfd1c048bca0004b60f79520e590200
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-28 08:14:29 +0100

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #ifdef MAIN_H
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -13,7 +13,5 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);'
-int _putchar(char);
 
 #endif
