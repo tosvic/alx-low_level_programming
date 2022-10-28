8850bbe0b28fc0d8b7c30b08516b093b31ccd284
tosvic <41272343+tosvic@users.noreply.github.com>
2022-10-28 09:59:18 +0100

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,24 +6,16 @@
  * @src: copy from
  * Return: pointer to dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int c, c2;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	c = 0;
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
+
 	return (dest);
 }
