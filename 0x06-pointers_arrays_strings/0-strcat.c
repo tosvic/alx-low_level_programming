f3b58e210857afc9d23db25bb65f1c0c4c37aa13
tosvic <victoruhuo@yahoo.com>
2022-10-28 01:39:20 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
 	j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
