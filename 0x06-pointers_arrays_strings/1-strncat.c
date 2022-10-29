743ba64b0a59c65c71cc1b4df37271146cc94cb0
tosvic <victoruhuo@yahoo.com>
2022-10-28 23:30:46 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0' && j < n)
+	while (*(dest + i) != '\0')
 	{
-		dest[i] = src[j];
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
 		i++;
 		j++;
 	}
-
-	dest[i] = '\0';
-
 	return (dest);
 }
