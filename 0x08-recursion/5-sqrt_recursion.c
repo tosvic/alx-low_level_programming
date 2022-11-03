1cef452ca3b294c7796cb55c010ec112bf7d61a2
tosvic <41272343+tosvic@users.noreply.github.com>
2022-11-03 05:49:50 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,61 +1,31 @@
 #include "main.h"
-
 /**
- *
- *  * helper - Finds the square root of two numbers
- *
- *   * @a: The number
- *
- *    * @b: The number to test for the square root of @a
- *
- *     * Return: square root
- *
- *      */
-
-int helper(int a, int b)
-
+ * helper - Finds the square root of two numbers
+ * @a: The number
+ * @b: The number to test for the square root of @a
+ * Return: square root
+ */
+int helper(int x, int y)
 {
+	if (y * y > a)
+		return (-1);
 
-		if (b * b > a)
+	else if (y * y == a)
+		return (y);
 
-					return (-1);
+	else
+		return (helper(x, y + 1));
 
-
-
-			else if (b * b == a)
-
-						return (b);
-
-
-
-				else
-
-							return (helper(a, b + 1));
-
-
-
-					return (1);
-
+	return (1);
 }
-
 /**
- *
- *  * _sqrt_recursion - returns the natural square root of a number
- *
- *   * @n: the number to return the square root of
- *
- *    * Return: the square root of @n
- *
- *     */
-
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
+ * Return: the square root of @n
+ */
 int _sqrt_recursion(int n)
-
 {
-
-		if (n == 0)
-
-					return (0);
-
-			return (helper(n, 1));
-
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
 }
