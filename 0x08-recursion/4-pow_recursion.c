f689d2a7d66e40f813d159bb4200cec0c6f7badb
tosvic <41272343+tosvic@users.noreply.github.com>
2022-11-03 05:48:32 +0100

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,31 +1,16 @@
 #include "main.h"
-
 /**
- *
- *  * _pow_recursion - returns the value of @x raised to the power of @y
- *
- *   * @x: number
- *
- *    * @y: the power to raise @n to
- *
- *     * Return: the value of @x to the @y power
- *
- *      */
-
-int _pow_recursion(int x, int y)
-
+ * _pow_recursion - returns the value of @x raised to the power of @y
+ * @x: number
+ * @y: the power to raise @n to
+ * Return: the value of @x to the @y power
+ */
+int _pow_recursion(int i, int j)
 {
+	if (j < 0)
+		return (-1);
+	if (j == 0)
+		return (1);
 
-		if (y < 0)
-
-					return (-1);
-
-			if (y == 0)
-
-						return (1);
-
-
-
-				return (x * (_pow_recursion(x, y - 1)));
-
+	return (i * (_pow_recursion(i, j - 1)));
 }
