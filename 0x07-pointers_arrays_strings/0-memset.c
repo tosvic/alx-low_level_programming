475a19698cd08453fa72956784d99d69caeb6ae1
tosvic <41272343+tosvic@users.noreply.github.com>
2022-11-11 04:03:48 +0100

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int counter;
 
-	for (i = 0; i < n; i++)
+	for (counter = 0; counter < n; counter++)
 	{
-		s(i) = b;
+		s[counter] = b;
 	}
 
 	return (s);
