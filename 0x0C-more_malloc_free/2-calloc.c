14d467268213c541aeffc2d8297a737410b95d73
tosvic <41272343+tosvic@users.noreply.github.com>
2022-11-11 03:42:09 +0100

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,89 +1,45 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include "main.h"
-
 /**
- *
- *  * _memset - copy char
- *
- *   * @s: string
- *
- *    * @b: input
- *
- *     * @n: bytes
- *
- *      * Return: string
- *
- *       */
-
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
 char *_memset(char *s, char b, unsigned int n)
-
 {
+	unsigned int i;
 
-		unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-
-
-			for (i = 0; i < n; i++)
-
-					{
-
-								s[i] = b;
-
-									}
-
-
-
-				return (s);
-
+	return (s);
 }
-
 /**
- *
- *  * _calloc - allocates memory for an array using malloc
- *
- *   * @nmemb: n elements
- *
- *    * @size: bytes
- *
- *     * Return: pointer
- *
- *      */
-
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
+	void *p;
 
-		void *p;
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
 
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	_memset(p, 0, (nmemb * size));
 
-
-			if (nmemb == 0 || size == 0)
-
-					{
-
-								return (NULL);
-
-									}
-
-				p = malloc(nmemb * size);
-
-
-
-					if (p == NULL)
-
-							{
-
-										return (NULL);
-
-											}
-
-						_memset(p, 0, (nmemb * size));
-
-
-
-							return (p);
-
+	return (p);
 }
