6942eaebb45bade41eae8e73ff3c5092608c54db
tosvic <victoruhuo@yahoo.com>
2022-11-27 14:16:29 -0800

    advan

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ *  * argstostr - prints args
+ *
+ *   * @ac: takes in width of grid
+ *
+ *    * @av: height of grid
+ *
+ *     * Return: the args one line at a time
+ *
+ *      */
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * count_word - helper function to count the number of words in a string
+ *
+ *   * @s: string to evaluate
+ *
+ *    *
+ *
+ *     * Return: number of words
+ *
+ *      */
